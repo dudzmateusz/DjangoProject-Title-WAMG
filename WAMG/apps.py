a6a6017e73ced67bd2d6f3af945a667abe013877
dudzmateusz <dudz.mateusz@gmail.com>
2022-01-12 18:55:37 +0100

    WAMG

--- WAMG/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps.config import *
-
-class mCConfig(AppConfig):
-    name = 'WAMG'
-    verbose_name = 'WAMG'
--- WAMG/tables.py
+++
@@ -1,8 +0,0 @@
-import django_tables2 as tables
-from .models import things
-
-class SomeTable(tables.Table):
-
-    class Meta:
-        model= things
-        attrs = {"class": "paleblue"}
